ba8758b5c778f349c96fd469eac89406c1691f36
roleqx <wizzsha@gmail.com>
2020-10-23 15:34:23 +0100

    Updated the Tailwind configuration file to prepare for the upcoming 2.0 changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 // config file
-// currently used only to enable the tailwind intellisense vscode extension
 module.exports = {
-  // important: true,
-  theme: {},
+  theme: {
+    // preps for tailwind 2.0
+    fontSize: {
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1.25rem" }],
+      base: ["1rem", { lineHeight: "1.5rem" }],
+      lg: ["1.125rem", { lineHeight: "1.75rem" }],
+      xl: ["1.25rem", { lineHeight: "1.75rem" }],
+      "2xl": ["1.5rem", { lineHeight: "2rem" }],
+      "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+      "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
+      "5xl": ["3rem", { lineHeight: "1" }],
+      "6xl": ["4rem", { lineHeight: "1" }],
+    },
+  },
   variants: {
     backgroundColor: ["responsive", "hover", "focus", "active"],
   },
-  plugins: [],
   future: {
     removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true,
   },
   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
-  // corePlugins: {
-  //   preflight: false,
-  // },
   prefix: "tw-",
 };
