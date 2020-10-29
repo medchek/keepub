f9ca4eb55408b88264d56f5afa67488b5d01f6fa
roleqx <wizzsha@gmail.com>
2020-10-29 16:06:06 +0100

    Fixed an issue with table of contents items component where it failed to render a single navigation element object instead of an array of objects

--- src/components/TocItems.vue
+++ src/components/TocItems.vue
@@ -1,20 +1,37 @@
 <template>
   <ul class="tw-h-full">
-    <li class="tw-w-full" v-for="nav in navPoint" :key="nav.attr_id">
+    <template v-if="Array.isArray(navPoint)">
+      <li class="tw-w-full" v-for="nav in navPoint" :key="nav.attr_id">
+        <a
+          class="tw-h-16 tw-w-full tw-flex tw-pl-4 tw-items-center hover:tw-bg-gray-700 tw-font-bold tw-border-b tw-border-gray-600 tw-border-opacity-25"
+          :href="nav.content.attr_src"
+          :class="appendClass"
+          @click.prevent="goToPage(nav.content.attr_src)"
+          >{{ decodeEntities(nav.navLabel.text) }}</a
+        >
+        <template v-if="nav.hasOwnProperty('navPoint')">
+          <TocItems
+            :nav-point="nav.navPoint"
+            append-class="tw-pl-10 tw-text-sm tw-italic"
+          />
+        </template>
+      </li>
+    </template>
+    <template v-else>
       <a
         class="tw-h-16 tw-w-full tw-flex tw-pl-4 tw-items-center hover:tw-bg-gray-700 tw-font-bold tw-border-b tw-border-gray-600 tw-border-opacity-25"
-        :href="nav.content.attr_src"
+        :href="navPoint.content.attr_src"
         :class="appendClass"
-        @click.prevent="goToPage(nav.content.attr_src)"
-        >{{ decodeEntities(nav.navLabel.text) }}</a
+        @click.prevent="goToPage(navPoint.content.attr_src)"
+        >{{ decodeEntities(navPoint.navLabel.text) }}</a
       >
-      <template v-if="nav.hasOwnProperty('navPoint')">
+      <template v-if="navPoint.hasOwnProperty('navPoint')">
         <TocItems
-          :nav-point="nav.navPoint"
+          :nav-point="navPoint.navPoint"
           append-class="tw-pl-10 tw-text-sm tw-italic"
         />
       </template>
-    </li>
+    </template>
   </ul>
 </template>
 
@@ -27,7 +44,7 @@ export default {
   name: "TocItems",
   props: {
     navPoint: {
-      type: Array,
+      type: [Array, Object],
       required: true,
     },
     appendClass: {
